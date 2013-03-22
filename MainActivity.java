208084cf632b0fe17f4cfd5c33cf9ad06a9f651d
vinil@riktamtech.com <vinil@riktamtech.com@f8818614-109f-7b55-5ab9-7509222c3d3b>
2013-03-22 09:49:16 +0000

    first commit

---
+++ MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.samplejsontester;
+
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.rikt.tech.and.jsonreceiver.JsonDataReceiver;
+
+public class MainActivity extends Activity {
+	JsonDataReceiver json;
+	JSONObject jsonObject;
+	TextView tv;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		json = new JsonDataReceiver(
+				"http://test.teamkollab.com:8080/SkyReach/Categories",
+				getApplicationContext());
+		jsonObject = json.getDataReceived();
+		tv = (TextView) findViewById(R.id.tv);
+		tv.setText(jsonObject.toString());
+	}
+
+}
